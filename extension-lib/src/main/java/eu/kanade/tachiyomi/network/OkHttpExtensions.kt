7e41935a098c9e593b26bda74709a0754a6fb883
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-06-20 15:52:59 +0500

    add some inline okhttp extensions

--- extension-lib/src/main/java/eu/kanade/tachiyomi/network/OkHttpExtensions.kt
+++ extension-lib/src/main/java/eu/kanade/tachiyomi/network/OkHttpExtensions.kt
@@ -1,7 +1,14 @@
 package eu.kanade.tachiyomi.network
 
+import okhttp3.CacheControl
 import okhttp3.Call
+import okhttp3.FormBody
+import okhttp3.Headers
+import okhttp3.HttpUrl
+import okhttp3.OkHttpClient
+import okhttp3.RequestBody
 import okhttp3.Response
+import kotlin.time.Duration.Companion.minutes
 
 suspend fun Call.await(): Response = throw Exception("Stub!")
 
@@ -18,3 +25,28 @@ suspend fun Call.awaitSuccess(): Response = throw Exception("Stub!")
  * @param code [Int] the HTTP status code
  */
 class HttpException(val code: Int) : IllegalStateException("HTTP error $code")
+
+suspend inline fun OkHttpClient.get(
+    url: String,
+    headers: Headers,
+    cache: CacheControl = CacheControl.Builder().maxAge(10.minutes).build()
+): Response {
+    return newCall(GET(url, headers, cache)).awaitSuccess()
+}
+
+suspend inline fun OkHttpClient.get(
+    url: HttpUrl,
+    headers: Headers,
+    cache: CacheControl = CacheControl.Builder().maxAge(10.minutes).build()
+): Response {
+    return newCall(GET(url, headers, cache)).awaitSuccess()
+}
+
+suspend inline fun OkHttpClient.post(
+    url: String,
+    headers: Headers,
+    body: RequestBody = FormBody.Builder().build(),
+    cache: CacheControl = CacheControl.Builder().maxAge(10.minutes).build()
+): Response {
+    return newCall(POST(url, headers, body, cache)).awaitSuccess()
+}
