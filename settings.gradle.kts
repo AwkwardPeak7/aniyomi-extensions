14c0c50a0c26fd6fb7cf7996a24ea19ea50c3ac4
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2024-06-21 16:37:33 +0500

    configure actions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,10 +8,36 @@ File(rootDir, "lib").eachDir { include("lib:${it.name}") }
 
 File(rootDir, "extractor").eachDir { include("extractor:${it.name}") }
 
-File(rootDir, "src").eachDir { dir ->
-    dir.eachDir { subdir ->
-        include("src:${dir.name}:${subdir.name}")
+
+if (System.getenv("CI") != "true") {
+    // Local development (full project build)
+
+    File(rootDir, "src").eachDir { dir ->
+        dir.eachDir { subdir ->
+            include("src:${dir.name}:${subdir.name}")
+        }
     }
+} else {
+    // Running in CI (GitHub Actions)
+
+    val chunkSize = System.getenv("CI_CHUNK_SIZE").toInt()
+    val chunk = System.getenv("CI_CHUNK_NUM").toInt()
+
+    // Loads individual extensions
+    File(rootDir, "src").getChunk(chunk, chunkSize)?.forEach {
+        include( "src:${it.parentFile.name}:${it.name}")
+    }
+}
+
+fun File.getChunk(chunk: Int, chunkSize: Int): List<File>? {
+    return listFiles()
+        ?.filter { it.isDirectory }
+        ?.mapNotNull { dir -> dir.listFiles()?.filter { it.isDirectory } }
+        ?.flatten()
+        ?.sortedBy { it.name }
+        ?.chunked(chunkSize)
+        ?.toList()
+        ?.get(chunk)
 }
 
 fun File.eachDir(block: (File) -> Unit) {
